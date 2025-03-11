b9fe0a5a3cd7356b224251339b41cac823e71cff
Alexey Ignatov <lexszero@gmail.com>
2025-03-11 03:51:33 +0200

    Add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 import uvicorn
 
 from power_map.api import power_map_api
 
 app = FastAPI(
     title="BL DoET data service"
+        )
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
         )
 
 app.include_router(power_map_api,
