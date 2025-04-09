1e8fe8449e9a297f8304fc9b8dcdd938a665d06a
Alexey Ignatov <lexszero@gmail.com>
2025-04-09 15:35:31 +0300

    ui: PlacementLayer: add icons to placement entity properties

--- doet-web-ui/src/routes/project/[slug]/PlacementLayer.svelte.ts
+++ doet-web-ui/src/routes/project/[slug]/PlacementLayer.svelte.ts
@@ -9,6 +9,13 @@ import { distance } from '$lib/utils';
 import { featureChip, InteractiveLayer, type InfoItem } from './InteractiveLayer.svelte';
 import { PowerGridLayer } from './PowerGridLayer.svelte';
 import { IconPlacementEntity, IconPDU, IconRuler, IconPower } from './Icons.svelte';
+import {
+  BadgeInfo as IconDescription,
+  Contact as IconContact,
+  Volume2 as IconSound,
+  PersonStanding as IconPeople,
+  Bus as IconVehicle
+} from '@lucide/svelte';
 
 const defaultStyle = {
   weight: 0.5,
@@ -106,11 +113,53 @@ export class PlacementLayer extends InteractiveLayer<
   featureProperties = (f: PlacementFeature) => {
     const props = f.properties;
     const result = [];
+
+    if (props.description) {
+      result.push({
+        label: 'Description',
+        value: props.description,
+        icon: IconDescription
+      });
+    }
+
+    if (props.contactInfo) {
+      result.push({
+        label: 'Contact',
+        value: props.contactInfo,
+        icon: IconContact
+      });
+    }
+
+    if (props.nrOfPeople) {
+      result.push({
+        label: '# people',
+        value: props.nrOfPeople,
+        icon: IconPeople
+      });
+    }
+
+    if (props.nrOfVechiles) {
+      result.push({
+        label: '# vehicles',
+        value: props.nrOfVechiles,
+        icon: IconVehicle
+      });
+    }
+
+    if ((props.amplifiedSound || 0)> 0) {
+      result.push({
+        label: 'Sound',
+        value: props.amplifiedSound,
+        icon: IconSound
+      });
+    }
+
     result.push({
       label: 'Power need',
       value: props.powerNeed,
       icon: IconPower
     });
+
 
     const near = this.getNearPDUs(f);
     //console.log(nearestPDU, nearestDistance);
@@ -124,7 +173,7 @@ export class PlacementLayer extends InteractiveLayer<
       });
     }
 
-    const exclude = ['name', 'type', '_nearPDUs', 'powerNeed'];
+    const exclude = ['type', 'name', 'description', 'contactInfo', 'nrOfPeople', 'nrOfVechiles', 'powerNeed', 'amplifiedSound', '_nearPDUs'];
     return [...result, ...(Object.entries(props)
       .filter(([k, v]) => (!exclude.includes(k)))
       .map(([k, v]) => ({label: k, value: v} as InfoItem))
